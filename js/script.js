13a5cbb6708c70d7e6cdda4c91b560c2bf45f870
ryan2135 <ryan2135@pacificu.edu>
2023-08-07 13:42:21 -0700

    Added jQuery examples

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+console.log("Script Loaded");
+
+function btnAlert () {
+    alert ("Button " + this.id + " Alert");
+}
+
+document.getElementById("btnButton1").onclick = btnAlert;
+document.getElementById("btnButton2").onclick = btnAlert;
+$("#btnButton3").on("click", btnAlert);
+$("#btnButton4").click(btnAlert);
