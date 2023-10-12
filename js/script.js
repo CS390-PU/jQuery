999d408144a98f6ea426fd42478379892b6dc115
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-10-12 04:08:08 +0000

    Added jQuery fade ...

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ document.getElementById("btnButton1").onclick = btnAlert;
 document.getElementById("btnButton2").addEventListener ('click', btnAlert);
 $("#btnButton3").on("click", btnAlert);
 $("#btnButton4").click(btnAlert);
+
+
+let $img = $("img");
+
+$("#show-btn").click(function() {
+   $img.show("normal");
+});
+
+$("#hide-btn").click(function() {
+   $img.hide("normal");
+});
+
+$("#toggle-btn").click(function() {
+   $img.toggle("normal");
+});
+
+$("#fadein-btn").click(function() {
+   $img.fadeIn("normal");
+});
+
+$("#fadeout-btn").click(function() {
+   $img.fadeOut("normal");
+});
